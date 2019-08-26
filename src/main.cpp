535de2528f2ed9c61938e130e6b37fd6260c1c8f
clinton <clinton.freeman@gmail.com>
2019-08-26 14:36:12 +1000

    Tidied up to just imu code.

--- src/main.cpp
+++ src/main.cpp
@@ -20,67 +20,85 @@
 #include <Wire.h>
 #include <MPU9250.h>
 
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid = "*********";      // EditThis: The name of your WiFi access point.
+const char* password = "*********";  // EditThis: The password of your WiFi access point.
+
 // SPARKFUN LIB HAS PEDOMETER INSIDE
 
-//MPU9250 IMU(Wire,0x68);
+MPU9250 IMU(Wire,0x68);
 
 void setup() {
 	Serial.begin(9600);
-	Wire.begin();
 
-	  // start communication with IMU
-  // int status = IMU.begin();
-  // if (status < 0) {
-  //   Serial.println("IMU initialization unsuccessful");
-  //   Serial.println("Check IMU wiring or try cycling power");
-  //   Serial.print("Status: ");
-  //   Serial.println(status);
-  //   while(1) {}
-  // }
-  // // setting the accelerometer full scale range to +/-8G
-  // IMU.setAccelRange(MPU9250::ACCEL_RANGE_8G);
-  // // setting the gyroscope full scale range to +/-500 deg/s
-  // IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
-  // // setting DLPF bandwidth to 20 Hz
-  // IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
-  // // setting SRD to 19 for a 50 Hz update rate
-  // IMU.setSrd(19);
+	// Initialise the IMU.
+  Serial.print("Initialising IMU...");
+  Wire.begin();
+  int status = IMU.begin();
+  if (status < 0) {
+    Serial.println("IMU initialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    Serial.print("Status: ");
+    Serial.println(status);
+    while(1) {}
+  }
+  // setting the accelerometer full scale range to +/-8G
+  IMU.setAccelRange(MPU9250::ACCEL_RANGE_8G);
+  // setting the gyroscope full scale range to +/-500 deg/s
+  IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
+  // setting DLPF bandwidth to 20 Hz
+  IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
+  // setting SRD to 19 for a 50 Hz update rate
+  IMU.setSrd(19);
+  Serial.println(" Done!");
+
+  // Connect to WiFi.
+  Serial.print("Connecting to Wifi");
+  // Prevent need for powercyle after upload.
+  WiFi.disconnect();
+
+  // Use DHCP to connect and obtain IP Address.
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  // Wait until we have connected to the WiFi AP.
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+  }
+
+  // Init UDP to broadcast OSC messages.
+  Udp.begin(localPort);
+  Serial.println(" Done!");
+
 }
 
 
-
 void loop() {
-	int heartRate = 60;  // Default the heart rate to a fairly average resting rate.
-  	int b = Wire.requestFrom(0xA0 >> 1, 1);
-  	if (b > 0) {
-    	heartRate = (int) Wire.read();
-  	}
+  IMU.readSensor();
 
-  	Serial.print("HR: ");
-  	Serial.println(heartRate);
+  // display the data
+  Serial.print(IMU.getAccelX_mss(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getAccelY_mss(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getAccelZ_mss(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getGyroX_rads(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getGyroY_rads(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getGyroZ_rads(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getMagX_uT(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getMagY_uT(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getMagZ_uT(),6);
+  Serial.print("\t");
+  Serial.println(IMU.getTemperature_C(),6);
 
-  // IMU.readSensor();
-
-  // // display the data
-  // Serial.print(IMU.getAccelX_mss(),6);
-  // Serial.print("\t");
-  // Serial.print(IMU.getAccelY_mss(),6);
-  // Serial.print("\t");
-  // Serial.print(IMU.getAccelZ_mss(),6);
-  // Serial.print("\t");
-  // Serial.print(IMU.getGyroX_rads(),6);
-  // Serial.print("\t");
-  // Serial.print(IMU.getGyroY_rads(),6);
-  // Serial.print("\t");
-  // Serial.print(IMU.getGyroZ_rads(),6);
-  // Serial.print("\t");
-  // Serial.print(IMU.getMagX_uT(),6);
-  // Serial.print("\t");
-  // Serial.print(IMU.getMagY_uT(),6);
-  // Serial.print("\t");
-  // Serial.print(IMU.getMagZ_uT(),6);
-  // Serial.print("\t");
-  // Serial.println(IMU.getTemperature_C(),6);
-
-  	delay(500);
+  delay(100);
 }
